131f8e849b0316ac73ee0b35c97f81ccae93d9d5
Pravallika <pkathi@usf.edu>
2025-10-02 22:00:39 -0400

    company class and employees

--- cc_6.js
+++ cc_6.js
@@ -9,3 +9,41 @@ class Employee {
   }
 }
 
+class Manager extends Employee {
+  constructor(name, department, teamSize) {
+    super(name, department);
+    this.teamSize = teamSize;
+  }
+
+  describe() {
+    return `${this.name} manages a team of ${this.teamSize} in ${this.department}`;
+  }
+}
+
+const mgr = new Manager("Pravallika Kathi", "Sales", 8);
+const mgr2 = new Employee("Brian Davis", "Human Resources");
+const mgr3 = new Employee("Carla Joseph", "IT");
+const mgr4 = new Employee("David Angel", "Operations");
+console.log(mgr.describe());
+
+const emp1 = new Employee("Sophia Brown", "Finance");
+const emp2 = new Employee("Michael Carter", "IT");
+const emp3 = new Employee("Emma Davis", "Marketing");
+const emp4 = new Employee("James Wilson", "Human Resources");
+console.log(emp1.describe());
+
+class Company {
+  constructor() {
+    this.employees = []; 
+  }
+
+  addEmployee(employee) { 
+    this.employees.push(employee);
+  }
+
+  listEmployees() {
+    this.employees.forEach(emp => {
+      console.log(emp.describe());
+    });
+  }
+}
